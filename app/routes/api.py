b2189ba52b0f18638cba9e34af2966607451de0f
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-02-23 15:22:47 -0800

    sessions added, redirecting logged in users from /login to /dashboard

---
+++ app/routes/api.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request, jsonify, session
+from app.models import User
+from app.db import get_db
+import sys
+bp = Blueprint('api', __name__, url_prefix='/api')
+
+@bp.route('/users', methods=['POST'])
+def signup():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # attempt to create a new user
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
+
+        db.add(newUser)
+        db.commit()
+    except:
+        # insert failed, so send errot to front end
+        print(sys.exe_info()[0])
+
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
+
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
+    return jsonify(id = newUser.id)
